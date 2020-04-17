5c57a82de04a312b9427d2150c321da7b3424bde
Thinh Hua <hquangthinh@gmail.com>
2020-04-17 19:11:13 +0700

    Add cookie support

--- lib/src/api_client/dio_factory.dart
+++ lib/src/api_client/dio_factory.dart
@@ -1,6 +1,9 @@
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 
 const int kConnectTimeout = 10000;
 const int kReceiveTimeout = 10000;
@@ -21,10 +24,10 @@ class AuthInterceptor extends Interceptor {
 }
 
 LogInterceptor createDebugLogInterceptor() => LogInterceptor(
-      requestHeader: true,
-      responseHeader: true,
-      requestBody: true,
-      responseBody: true,
+      requestHeader: false,
+      responseHeader: false,
+      requestBody: false,
+      responseBody: false,
     );
 
 LogInterceptor createInfoLogInterceptor() => LogInterceptor(
@@ -37,12 +40,34 @@ LogInterceptor createInfoLogInterceptor() => LogInterceptor(
 
 class DioFactory {
   static DioCacheManager _cacheManager;
+  static CookieManager _cookieManager;
+  static String _tempCookieDir;
+
+  static Future<void> initAsync() async {
+    final tempDir = await getTemporaryDirectory();
+    _tempCookieDir = tempDir.path;
+  }
+
+  static void initWithTempDir(String tempDir) {
+    _tempCookieDir = tempDir;
+  }
 
   static DioCacheManager getCacheManager(String baseUrl) {
-    if (null == _cacheManager) {
+    if (_cacheManager == null) {
+      print('********************* cache manager is null create it **************************');
       _cacheManager = DioCacheManager(CacheConfig(baseUrl: baseUrl));
     }
     return _cacheManager;
+  }
+
+  static CookieManager getCookieManager() {
+    if(_cookieManager == null) {
+      print('********************* getCookieManager manager is null create it **************************');
+      print(_tempCookieDir);
+      assert(_tempCookieDir != null && _tempCookieDir.isNotEmpty, 'Cookie directory is not initialized.');
+      _cookieManager = CookieManager(PersistCookieJar(dir: _tempCookieDir));
+    }
+    return _cookieManager;
   }
 
   static Dio createPublicClient({
@@ -59,7 +84,8 @@ class DioFactory {
         contentType: contentType,
         headers: {'Accept': contentType},
       )
-      ..interceptors.add(LogInterceptor(responseBody: true, requestBody: true))
+      ..interceptors.add(getCookieManager())
+      ..interceptors.add(createDebugLogInterceptor())
       ..interceptors.add(InterceptorsWrapper(onError: (DioError e) {
         print(e);
         return e;
@@ -83,6 +109,7 @@ class DioFactory {
         headers: {'Accept': contentType},
       )
       ..interceptors.add(AuthInterceptor(accessToken))
+      ..interceptors.add(getCookieManager())
       ..interceptors.add(createDebugLogInterceptor())
       ..interceptors.add(InterceptorsWrapper(onError: (DioError e) {
         print(e);
@@ -110,6 +137,7 @@ class DioFactory {
               .extra)
       ..interceptors.add(createDebugLogInterceptor())
       ..interceptors.add(getCacheManager(apiBaseUrl).interceptor)
+      ..interceptors.add(getCookieManager())
       ..interceptors.add(InterceptorsWrapper(onError: (DioError e) {
         print(e);
         return e;
@@ -138,6 +166,7 @@ class DioFactory {
       ..interceptors.add(AuthInterceptor(accessToken))
       ..interceptors.add(createDebugLogInterceptor())
       ..interceptors.add(getCacheManager(apiBaseUrl).interceptor)
+      ..interceptors.add(getCookieManager())
       ..interceptors.add(InterceptorsWrapper(onError: (DioError e) {
         print(e);
         return e;
